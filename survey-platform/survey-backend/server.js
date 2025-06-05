b0e475b07b6254cb157fbbb4d4d90a25aa894fed
Vikas <singhvikas.sv30@gmail.com>
2025-06-05 13:03:10 +0000

    .env. server

---
+++ survey-platform/survey-backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/v1/auth', authRoutes);
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on http://localhost:${process.env.PORT}`)
+    );
+  })
+  .catch(err => console.error(err));
